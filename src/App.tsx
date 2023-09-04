14c12b69afd52c1a1781ce20e9b09bdb60be51b4
Alex Vido <alex.vido93@gmail.com>
2023-09-04 13:13:45 -0300

    Modified project to deploy in vercel

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Login from './components/login';
 import Search from './components/search';
 import Album from './components/album';
@@ -10,20 +10,17 @@ import NotFound from './components/not_found';
 
 function App() {
   return (
-    <Router basename={ process.env.PUBLIC_URL }>
-      <Routes>
-        <Route index element={ <Login /> } />
-        <Route path="/trybetunes" element={ <Layout /> }>
-          <Route path="/search" element={ <Search /> } />
-          <Route path="/album/:id" element={ <Album /> } />
-          <Route path="/favorites" element={ <Favorites /> } />
-          <Route path="/profile" element={ <Profile /> } />
-          <Route path="/profile/edit" element={ <ProfileEdit /> } />
-          <Route path="/*" element={ <NotFound /> } />
-        </Route>
-      </Routes>
-    </Router>
-
+    <Routes>
+      <Route index element={ <Login /> } />
+      <Route path="/" element={ <Layout /> }>
+        <Route path="/search" element={ <Search /> } />
+        <Route path="/album/:id" element={ <Album /> } />
+        <Route path="/favorites" element={ <Favorites /> } />
+        <Route path="/profile" element={ <Profile /> } />
+        <Route path="/profile/edit" element={ <ProfileEdit /> } />
+        <Route path="/*" element={ <NotFound /> } />
+      </Route>
+    </Routes>
   );
 }
 
--- vite.config.ts
+++ vite.config.ts
@@ -18,5 +18,4 @@ export default defineConfig({
       provider: 'istanbul'
     }
   },
-  base: "/trybetunes"
 })
